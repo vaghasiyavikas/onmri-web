b5916a3fa657cb2100969ee88bf7383ec075a7e9
Vikas Vaghasiya <vikasvaghasiya1212@gmail.com>
2025-11-10 10:15:45 +0000

    Add tag script

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5FFSTS3G');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="canonical" href="https://onmri.com" />
         <meta name="geo.region" content="GB" />
         <meta name="geo.placename" content="Sleaford, United Kingdom" />
@@ -60,7 +71,19 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5FFSTS3G"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
